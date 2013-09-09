42c86643d8704a24cff66bd5c0908e17cb1b4d7b
gogoganus <egor.vodnev@gmail.com>
2013-09-09 11:59:47 +0700

    Add built scripts

---
+++ helloworld/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Егор
+ *
+ * Created on 9 Сентябрь 2013 г., 11:51
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
