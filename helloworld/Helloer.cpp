1cf8b68225a85f01d672dee749142f0b5f1ad09c
gogoganus <egor.vodnev@gmail.com>
2013-09-10 01:17:13 +0700

    New class 'Helloer' was added in the project

---
+++ helloworld/Helloer.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Helloer.cpp
+ * Author: Егор
+ * 
+ * Created on 10 Сентябрь 2013 г., 1:15
+ */
+
+#include "Helloer.h"
+
+Helloer::Helloer() {
+}
+
+Helloer::Helloer(const Helloer& orig) {
+}
+
+Helloer::~Helloer() {
+}
+
